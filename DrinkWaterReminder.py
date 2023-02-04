34ec2576ea58e7f15dc24f93b173a2c3ee4c809b
Ishaan Sharma <talktoishaan@hotmail.com>
2023-02-04 15:15:21 +0530

    first commit

---
+++ DrinkWaterReminder.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification  
+
+if __name__ == "__main__":
+  
+        notification.notify(
+        title = "DRINK WATER!",
+        message = "A healthy adult body requires 3-3.7L of water to work efficiently!",
+        #app_icon = "C:\Users\Ishaan Sharma\Desktop\Desktop clutter\Python\icon.ico",
+        timeout = 10
+    )
+ 
+    
